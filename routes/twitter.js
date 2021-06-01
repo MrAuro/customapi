5179a15c6be2d767eafdfa4d2672ef483224ba90
Auro <35087590+MrAuro@users.noreply.github.com>
2021-06-01 17:01:54 -0400

    Set count to 3 for tweets in case the first tweet isnt found...?

--- routes/twitter.js
+++ routes/twitter.js
@@ -23,13 +23,16 @@ let T = new Twit({
 */
 router.route('/latest/:username').get((req, res) => {
     // Check if there is a username that fits the regex
-    let targetUser = req.params.username.match(/(^|[^@\w])(\w{1,15})\b/);
+    let targetUser = req.params.username.match(/^(@|)[A-z0-9]{1,15}$/);
     if (!targetUser) return req.json('Invalid username');
+    targetUser = targetUser[0].replace('@', '');
 
     // Use Twit to fetch the twiter api
-    T.get('search/tweets', { q: `from:${req.params.username}`, count: 1, tweet_mode: 'extended', attachment_url: true }, function (err, data, response) {
+    T.get('search/tweets', { q: `from:${targetUser}`, count: 3, tweet_mode: 'extended' }, function (err, data, response) {
         if (response.statusCode != 200) return res.sendStatus(response.statusCode);
         if (data.statuses.length == 0) return res.json('');
+
+        // console.log(response);
 
         if (req.query?.noping === 'true') {
             // Zero width character to prevent pings
@@ -42,12 +45,26 @@ router.route('/latest/:username').get((req, res) => {
                     return match[0] + match[1] + zeroWidth + match[2];
                 })
                 .replace(/\n/g, ' ');
-            res.json(`${cleanedTweet} | https://twitter.com/${data.statuses[0].user.screen_name}/status/${data.statuses[0].id_str}`);
+            res.json(`${unescapeHTML(cleanedTweet)} | https://twitter.com/${data.statuses[0].user.screen_name}/status/${data.statuses[0].id_str}`);
         } else {
             let latestTweet = data.statuses[0].full_text.replace(/(\n)/g, ' ');
-            res.json(`${latestTweet} | https://twitter.com/${data.statuses[0].user.screen_name}/status/${data.statuses[0].id_str}`);
+            res.json(`${unescapeHTML(latestTweet)} | https://twitter.com/${data.statuses[0].user.screen_name}/status/${data.statuses[0].id_str}`);
         }
     });
 });
 
 module.exports = router;
+
+// https://www.30secondsofcode.org/js/s/unescape-html
+const unescapeHTML = (str) =>
+    str.replace(
+        /&amp;|&lt;|&gt;|&#39;|&quot;/g,
+        (tag) =>
+            ({
+                '&amp;': '&',
+                '&lt;': '<',
+                '&gt;': '>',
+                '&#39;': "'",
+                '&quot;': '"',
+            }[tag] || tag)
+    );
